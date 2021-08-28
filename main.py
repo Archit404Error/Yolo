bd5c9554bda36435003870b2dd386c44a53b8656
Archit404Error <4architmehta@gmail.com>
2021-08-28 17:05:55 -0400

    Switched to python

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+import mysql.connector
+import requests
+
+app = Flask(__name__)
+
+#graph_api_url = "http://localhost:4000/graphql"
+
+mydb = mysql.connector.connect(
+  host="sql5.freesqldatabase.com",
+  user="sql5432499",
+  password="Lp5Tw8l2GR",
+  database="sql5432499"
+ )
+
+cursor = mydb.cursor()
+
+@app.route('/query')
+def queryDB():
+    '''
+    query = """
+        query {
+            events {
+                id,
+                image,
+                title,
+                description,
+                location
+            }
+        }
+    """
+    res = requests.post(graph_api_url, json  = { 'query' : query })
+    return jsonify(res.text)
+    '''
+    cursor.execute("SELECT * FROM Events")
+    res = cursor.fetchall()
+    res_arr = []
+    for val in res:
+        res_arr.append(val)
+    return jsonify(res_arr)
+
+app.run(debug = True)
