95b60f04a6b2204176fb358c1c8b16084d3730b1
Archit404Error <4architmehta@gmail.com>
2022-04-02 19:16:51 -0400

    Add event suggestion cronjob and small event invite bugfix

--- suggestionEngines/eventSuggestionEngine.js
+++ suggestionEngines/eventSuggestionEngine.js
@@ -1,4 +1,8 @@
-export const populateEventSuggestions = (userId) => {
+import { ObjectId } from "mongodb";
+import { calculateTagWeights, calculateOrganizerWeights } from "../helperMethods.js";
+import { getAllUserIds } from "./suggestionHelpers.js";
+
+export const populateEventSuggestions = async (userCollection, eventCollection, userId) => {
     const userDoc = await userCollection.findOne({ "_id": userId });
     let tagWeights = {};
     let organizerWeights = {};
@@ -7,6 +11,7 @@ export const populateEventSuggestions = (userId) => {
     // Find most accepted tags
     tagWeights = calculateTagWeights(await userDoc)
 
+    // Find most accepted organizers
     organizerWeights = calculateOrganizerWeights(await userDoc)
 
     // Find most similar attended events by people who attended this event
@@ -16,7 +21,11 @@ export const populateEventSuggestions = (userId) => {
 
     await eventCollection.find({
         _id: {
-            $nin: await userDoc.acceptedEvents.map(event => event._id)
+            $nin: ([
+                await userDoc.acceptedEvents.map(event => event._id),
+                await userDoc.pendingEvents,
+                await userDoc.rejectedEvents
+            ].flat())
         },
         endDate: {
             $gte: new Date()
@@ -37,11 +46,21 @@ export const populateEventSuggestions = (userId) => {
     const topRec = Object.entries(finalWeights)
         .sort(([, a], [, b]) => a - b)
         .map(freqArr => freqArr[0])
-        .filter((elem, index) => index < 5)
+        .filter((_, index) => index < 5)
         .map(rec => new ObjectId(rec))
 
     userCollection.updateOne(
         { "_id": new ObjectId(userId) },
         { $push: { "pendingEvents": { $each: topRec } } }
     )
+
+    return topRec
+}
+
+export const populateAllEventSuggestions = async (userCollection, eventCollection) => {
+    console.log("populating...")
+    const users = await getAllUserIds(userCollection);
+
+    for (const user of await users)
+        populateEventSuggestions(userCollection, eventCollection, user)
 }
